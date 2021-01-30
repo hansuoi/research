5dbed1dce7f1bc48f7b0746efea2dbffb53baf4b
hayashireirai@yahoo.co.jp <hayashireirai@yahoo.co.jp>
2021-01-30 13:51:19 +0900

    first commit

--- szk_lib.py
+++ szk_lib.py
@@ -1,7 +1,7 @@
 def mecab_list(text):
     import MeCab
     tagger = MeCab.Tagger("-Ochasen")
-    tagger.parse('')    # よくわかんないけどこうするとエラーが出ない
+    tagger.parse('')
     node = tagger.parseToNode(text)
     word_class = []
     while node:
@@ -11,7 +11,7 @@ def mecab_list(text):
 
         if wclass[0] != u'BOS/EOS':
             if wclass[6] == None:
-                word_class.append((word,wclass[0],""))
+                word_class.append((word,wclass[0],word))
             else:
                 word_class.append((word,wclass[0],wclass[6]))
         node = node.next
@@ -20,48 +20,87 @@ def mecab_list(text):
     return word_class
 
 
-# 入力された単語の品詞を返す
-def hinshi_check(word):
-    return mecab_list(word)[0][1]
-
-
-# 入力された単語の原型を返す
+# 入力された単語の原型を返す(改善版)
 def genkei(word):
-    return mecab_list(word)[0][2]
+    import MeCab
+    tagger = MeCab.Tagger("-Ochasen")
+    tagger.parse('')
+    node = tagger.parseToNode(word)
+
+    while node:
+        w = node.surface
+        wclass = node.feature.split(',')
+        # wclass:品詞,品詞細分類1,品詞細分類2,品詞細分類3,活用形,活用型,原形,読み,発音
+
+        if wclass[0] != u'BOS/EOS':
+            if wclass[6] == None:
+                return w
+            else:
+                return wclass[6]
+        node = node.next
+
+
+# 入力された単語の品詞を返す(改善版)
+def hinshi(word):
+    import MeCab
+    tagger = MeCab.Tagger("-Ochasen")
+    tagger.parse('')
+    node = tagger.parseToNode(genkei(word))
+
+    while node:
+        word = node.surface
+        wclass = node.feature.split(',')
+        # wclass:品詞,品詞細分類1,品詞細分類2,品詞細分類3,活用形,活用型,原形,読み,発音
+
+        if wclass[0] != u'BOS/EOS':
+            return wclass[0]
+        node = node.next
+
+
+# 登場した単語(原形)のリストを作る
+def words_list(text):
+    words = list(set(text.split()))
+
+    for i in range(len(words)):
+        words[i] = genkei(words[i])
+
+    words = list(set(words))
+
+    import pickle
+    with open('words_list', mode='wb') as f:
+        pickle.dump(words , f)
 
 
 # 共起行列(と単語のリスト)を返す
 # w_size: ウィンドウ幅
-def cooccurrence(text, w_size):
+def c_matrix(text, w_size, w_list):
     import numpy as np
+    import pickle
 
     # i文目のj語目が格納された二次元配列sentence(i, j)
     sentence = text.split('\n')
     for i in range(len(sentence)):
         sentence[i] = sentence[i].split()
 
-    # 登場した単語のリスト
-    words = list(set(text.split()))
+    # 登場した単語(原形)のリスト
+    with open(w_list, mode='rb') as f:
+        words = pickle.load(f)
 
     # words数サイズのゼロ行列を作る
     matrix = np.zeros((len(words), len(words)))
 
-    # number番目の単語がw
-    for number, w in enumerate(words):
-        for i in range(len(sentence)):
-            for j in range(len(sentence[i])):
-                # wとi文目j単語目とが合致したら
-                if w == sentence[i][j]:
-                    # 行列の、窓幅分前の単語のところを+1する
-                    for d in range(1, w_size+1):
-                        if j-d >= 0:
-                            matrix[number][words.index(sentence[i][j-d])] += 1
-                    # 行列の、窓幅分後の単語のところを+1する
-                    for d in range(1, w_size+1):
-                        if j+d < len(sentence[i]):
-                            matrix[number][words.index(sentence[i][j+d])] += 1
+    for i in range(len(sentence)):
+        for j in range(len(sentence[i])):
+            number = words.index(genkei(sentence[i][j]))
+            # 行列の、窓幅分前/後の単語の部分を+1する
+            for d in range(1, w_size+1):
+                if j-d >= 0:
+                    matrix[number][words.index(genkei(sentence[i][j-d]))] += 1
+                if j+d < len(sentence[i]):
+                    matrix[number][words.index(genkei(sentence[i][j+d]))] += 1
 
-    return words, matrix
+    with open('c_matrix', mode='wb') as f:
+        pickle.dump(matrix , f)
 
 
 # ベクトルの0でない成分を返す
@@ -88,39 +127,39 @@ def WeedsP(w1, w2):
     return c / m
 
 
-def weeds_list(text, w_size=1):
-    # words: textに登場する単語のリスト
+def weeds_list(text, w_list, c_matrix):
+    import pickle
+
+    # 登場した単語(原形)のリスト
+    with open(w_list, mode='rb') as f:
+        words = pickle.load(f)
+
     # matrix: 共起行列
-    words, matrix = cooccurrence(text, w_size=1)
+    with open(c_matrix, mode='rb') as f:
+        matrix = pickle.load(f)
 
-    # 下位語候補, 上位語候補, weeds値
-    # を格納する配列を用意しておく
-    result = []
-
+    # words[i]が下位・words[j]が上位、と仮定してweedsを計算する
     for i in range(len(words)):
         for j in range(len(words)):
-            if i == j:
-                continue
-            else:
-                result.append([words[i], words[j], WeedsP(matrix[i], matrix[j])])
-
-    # weeds値が大きい順(上位・下位関係がはっきりしている順)に並べ替える
-    result = sorted(result, reverse=True, key=lambda x: x[2])
-
-    return result
+            if i!=j and hinshi(words[i])=='名詞' and hinshi(words[j])=='名詞':
+                w = WeedsP(matrix[i], matrix[j])
+                if w >= 0.5:
+                    with open('weeds.txt', mode='a', encoding='utf-8') as f:
+                        f.write(', '.join([words[i], words[j], str(w)]))
+                        f.write('\n')
 
 
 # 記号を削除する
-def preprocess1(text):
-    hinshi_check
-    #記号を消すコード
+def del_kigo(text):
+    import string
+    kigo = string.punctuation + '❶' + '②' + '｣' + '③' + '❹' + '●' + '■'
+    result = text.translate(str.maketrans('', '', kigo))
+    return result
 
 
 # 名詞が連続してたら名詞句として扱う(わかち書かない)
 # 名詞・名詞句以外は削除する
 def preprocess2(text):
-    from szk_lib import hinshi_check
-
     # 名詞と名詞句とを格納する配列
     nouns = []
 
@@ -132,9 +171,9 @@ def preprocess2(text):
     for i in range(len(sentence)):
         j = 0
         while j < len(sentence[i]):
-            if hinshi_check(sentence[i][j]) == '名詞':
+            if hinshi(sentence[i][j]) == '名詞':
                 w = ''
-                while j<len(sentence[i]) and hinshi_check(sentence[i][j])=='名詞':
+                while j<len(sentence[i]) and hinshi(sentence[i][j])=='名詞':
                     w += sentence[i][j]
                     j += 1
                 nouns.append(w)
@@ -157,17 +196,14 @@ def substr(text):
     # textから名詞と名詞句だけ抽出
     nouns = preprocess2(text)
 
-    # result: 共通部分, s1, s2
-    result = []
-
+    # 「s1,s2の共通部分, s1, s2」をtxtに保存
     for i in range(len(nouns)):
         for j in range(i+1, len(nouns)):
-            result.append([find_longest_substr(nouns[i], nouns[j]), nouns[i], nouns[j]])
-
-    # 共通部分順に並べ替える
-    result = sorted(result, reverse=True, key=lambda x: x[0])
-
-    return result
+            c = find_longest_substr(nouns[i], nouns[j])
+            if len(c) >= 2:
+                with open('共通部分.txt', mode='a', encoding='utf-8') as f:
+                    f.write(', '.join([c, nouns[i], nouns[j]]))
+                    f.write('\n')
 
 
 # pdfをtxtに変換する
@@ -191,3 +227,28 @@ def unicode_nfkc(file_name):
     text = unicodedata.normalize("NFKC",text)
     with open(file_name+'.txt', mode='w', encoding='utf-8') as f:
         f.write(text)
+
+
+# pdfからtxtに変換し、正規化・記号削除
+def preprocess1(file_name):
+    pdf_to_txt(file_name)
+    unicode_nfkc(file_name)
+
+    with open(file_name+'.txt', mode='r', encoding='utf-8') as f:
+        text = f.read()
+
+    text = del_kigo(text)
+
+    import MeCab
+    tagger = MeCab.Tagger("-Owakati")
+    tagger.parse('')
+
+    text = text.split('\n')
+    for t in text:
+        with open(file_name+'_w.txt', mode='a', encoding='utf-8') as f:
+            f.write(tagger.parse(t))
+
+    with open(file_name+'_w.txt', mode='r', encoding='utf-8') as f:
+        text = f.read()
+
+    return text
